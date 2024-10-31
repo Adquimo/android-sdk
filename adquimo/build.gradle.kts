06461576b06b591b8ba10a4e62806267fa9ed7b1
blackhofc <benjax48@hotmail.com.ar>
2024-10-31 11:50:15 -0300

    Latest fix build.gradle

--- adquimo/build.gradle.kts
+++ adquimo/build.gradle.kts
@@ -39,10 +39,21 @@ android {
 
 publishing {
     publications {
+        // Register the release publication for publishing
         register<MavenPublication>("release") {
+            groupId = "com.github.adquimo"
+            artifactId = "android-sdk"
+            version = "1.0.1"
+
+            // Include the release component from the library module
             afterEvaluate {
                 from(components["release"])
             }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io") // JitPack repository
         }
     }
 }
